5dc1534636792580c11e131531e5220380197b90
Terry Dutcher <terryinclmich@hotmail.com>
2022-03-17 09:02:46 -0600

    Finished Video 14 Context Dictionary

--- my_app/todo_list/views.py
+++ my_app/todo_list/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# The python dictionary which is the last parameter in the render can be used to pass data
+# to our html pages.
+
 def home(request):
     return render(request, 'home.html', {})
 
 def about(request):
-    return render(request, 'about.html', {})
+    # Call using full name
+    # my_name = 'Terry Dutcher'
+    # return render(request, 'about.html', {'name': my_name})
+   
+    # Call using multiple variables
+    # my_name = 'Terry'
+    # return render(request, 'about.html', {'first_name': my_name, 'last_name': 'Dutcher'})
+   
+    # Create the dictionary seperately - good for readability. 
+    context = {'first_name': 'Terry', 'last_name': 'Dutcher'}
+    return render(request, 'about.html', context)
